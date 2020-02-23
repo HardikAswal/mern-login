4f9b5cc3267d67e0133d2bfdb6000140b1f1757e
HardikAswal <hardik.aswal@gmail.com>
2020-02-23 23:26:49 +0530

    ReactJS Login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username:"",
+      email:""
+    }
+  
+  }
+
+  handleSubmit(){
+    console.log(this.state.username);
+    console.log(this.state.password);
+
+    var auth = {
+      username:this.state.username,
+      password:this.state.password
+    }
+
+    axios.post('http://localhost:8080/login',auth)
+    .then(res => {
+        alert(res.data.message);
+      })
+    .catch(err => {
+      console.log(err)
+    })  
+
+    
+  }
+
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="text" name="username" placeholder="username" onChange={(e)=>this.setState({username:e.target.value})} />
+        <input type="password" name="password" placeholder="password" onChange={(e)=>this.setState({password:e.target.value})}/>
+        <button type="submit" onClick={(e)=>this.handleSubmit(e)} >Login</button>     
     </div>
-  );
+  )}
 }
 
 export default App;
